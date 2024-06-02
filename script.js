a24ebbdf0525521fcffb5e35a8232bb6daa8c53b
thesoumisamanta <samantasoumi10@gmail.com>
2024-06-02 00:48:59 -0500

    add responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-// let menuIcon = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
+//Intro animation
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#element', {
+        strings: ['Web Developer', 'Web Designer', 'Video Editor'],
+        typeSpeed: 50,
+        loop: true
+    });
+});
 
-// menuIcon.onclick = () => {
-//     menuIcon.classList.toogle('')
-// }
+
+
+//Navbar Target
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetClass = this.getAttribute('href').substring(1);
+        const targetElement = document.querySelector('.' + targetClass);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+
+// Toggle Menu
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('show');
+
+});
